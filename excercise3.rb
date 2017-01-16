28d724aeae3496be27d88c4d0adde50c243d94e7
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-16 11:59:25 -0500

    getting the age of the user.

--- excercise3.rb
+++ excercise3.rb
@@ -1,3 +1,8 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+
+puts "I'd like to know your age. How old are you?"
+age = gets.chomp
+dateofbirth = 2017 - age.to_i
+puts "You were born in #{dateofbirth}."
