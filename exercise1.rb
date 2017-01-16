cab958aa1b767cd0ea8f6bfd1afc7899dc414dbc
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-16 11:37:03 -0500

    finish excercise1

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,15 @@
 puts 2
 puts 3
 puts 2 + 3
+
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
+
+
+puts "hello"+"world"
+
+puts (1 < 3) && (2 < 3)
+puts (2 != 1) || (2 > 3)
