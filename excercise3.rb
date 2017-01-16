cce7a27e70def1bced775906911276e94074d0db
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-16 12:06:44 -0500

    excercise3 finished.

--- excercise3.rb
+++ excercise3.rb
@@ -6,3 +6,8 @@ puts "I'd like to know your age. How old are you?"
 age = gets.chomp
 dateofbirth = 2017 - age.to_i
 puts "You were born in #{dateofbirth}."
+
+
+one_to_ten = (1..10)
+
+one_to_ten.each {|num| puts (num**2).to_s}
