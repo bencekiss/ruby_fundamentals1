13da72766f26e4b8e4a8363cc8c839d0797dbf42
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-16 11:51:10 -0500

    excercise 2 finished

---
+++ exercise2.rb
@@ -0,0 +1,6 @@
+meal = 55
+tip = meal * 0.15
+puts "A tip for a meal that costs $#{meal} is $#{tip}."
+
+puts "The result of " + 45628.to_s + " multiplied by " + 7839.to_s + " is " + (45628 * 7839).to_s + "."
+puts (10 < 20 && 30 < 20) || !(10 == 11)
